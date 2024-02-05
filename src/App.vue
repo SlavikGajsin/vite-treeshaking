fe742930a0793f00844e1f0fbf7d8eb210eb558f
Vyacheslav <vgaysin@abyka.com>
2024-02-06 01:02:34 +0500

    .

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {
+  cloneDeep,
+  //   isEqual,
+  // merge
+} from "lodash-es";
+
+const objectToClone = {
+  a: 'a',
+  b: 'b',
+  c: 'c',
+  d: {
+    a: 'a',
+    b: 'b',
+    c: 'c',
+  }
+}
+
+const cloned = cloneDeep(objectToClone)
+console.log(cloned)
+// const merged = merge(objectToClone, cloned)
+//
+// console.log(isEqual(cloned, objectToClone))
+
+// console.log(merged)
+</script>
+
+<template>
+  some text
+</template>
